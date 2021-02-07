dfa07820a48f1a8a013420cbc3ce63dd1ddc38f8
SeoSang <ddrrpg@naver.com>
2021-02-07 19:34:47 +0900

    Add User model

--- backend/leads/models.py
+++ backend/leads/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  SEX_CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female')
+  )
+  
+  age = models.IntegerField(default=0)
+  sex = models.CharField(choices=SEX_CHOICES, max_length=20)
+  email = models.EmailField(max_length=100, unique=True)
+  password = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=20)
+  created_at = models.DateField(auto_now=True)
+  first_name = models.CharField(max_length=20)
+  profile_img = models.ImageField()
+  
+  # Metadata
+  class Meta:
+      ordering = ['id']
+
+  def __str__(self):
+      """String for representing the MyModelName object (in Admin site etc.)."""
+      return self.first_name + self.last_name
+  
